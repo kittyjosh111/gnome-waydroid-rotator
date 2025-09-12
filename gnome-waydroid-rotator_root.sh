c645514f2ef5c7c6f8952b571bcad8e5e5c9e995
herstalmakrov4219 <joshua.shi@gmail.com>
2025-09-11 21:29:11 -0700

    redo logic for root script, REQUIRES systemctl now

--- gnome-waydroid-rotator_root.sh
+++ gnome-waydroid-rotator_root.sh
@@ -6,21 +6,17 @@
 ## Polling is expensive. Let us use inotify.
 ## also, we don't do any checking lmao xd
 
-if [ -f "/tmp/gnome-waydroid-rotator.now" ]; then
-  inotifywait --monitor --format "%e %w%f" --event modify,create /tmp/gnome-waydroid-rotator.now | while read changed; do
-    rot_value=$(cat /tmp/gnome-waydroid-rotator.now)
-    waydroid shell wm user-rotation lock $rot_value
-  done
-else
+if [ ! -f "/tmp/gnome-waydroid-rotator.now" ]; then
   echo "/tmp/gnome-waydroid-rotator.now not found. Waiting..."
   inotifywait --monitor --format '%f' --event modify,create /tmp/ | while read -r filename; do
     if [ "$filename" == "gnome-waydroid-rotator.now" ]; then
-      echo "/tmp/gnome-waydroid-rotator.now found!"
-      break 
+      echo "/tmp/gnome-waydroid-rotator.now found! Restarting service..."
+      systemctl restart gnome-waydroid-rotator_root
     fi
   done
-  inotifywait --monitor --format "%e %w%f" --event modify,create /tmp/gnome-waydroid-rotator.now | while read changed; do
-    rot_value=$(cat /tmp/gnome-waydroid-rotator.now)
-    waydroid shell wm user-rotation lock $rot_value
-  done
 fi
+
+inotifywait --monitor --format "%e %w%f" --event modify,create /tmp/gnome-waydroid-rotator.now | while read changed; do
+  rot_value=$(cat /tmp/gnome-waydroid-rotator.now)
+  waydroid shell wm user-rotation lock $rot_value
+done
